1081d8b54e8652c5612f5a382ff336b88a6d8607
Abraham <wole1702@gmail.com>
2023-10-31 21:51:00 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 100 - 1:
-        print('{:02d}'.format(i), end=',', sep=',')
+        print('{:02d}'.format(i), end=', ', sep='')
     else:
         print('{:02d}'.format(i))
