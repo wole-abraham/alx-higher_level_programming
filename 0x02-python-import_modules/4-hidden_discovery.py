8a17977c14e7e1b99fb6c43b8f2ca3d60944e24a
Abraham <wole1702@gmail.com>
2023-11-05 16:13:13 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-names = dir(hidden_4)
-names.sort()
-for name in names:
-    if '__' not in name:
-        print(name)
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    names.sort()
+    for name in names:
+        if '__' not in name:
+            print(name)
