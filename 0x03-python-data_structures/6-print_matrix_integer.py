9cdaf32904d88ce08cf955f41c5ae6245b970b0f
Abraham <wole1702@gmail.com>
2023-11-07 22:58:22 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0 or matrix is None:
-        return (None)
+        return matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j == len(matrix[i]) - 1:
