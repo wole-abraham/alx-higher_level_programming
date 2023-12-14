11f3cf8d318a5c9f5f88b7776da132dbba855e0a
Abraham <wole1702@gmail.com>
2023-12-14 02:58:35 +0100

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""_summary_"""
+import json
+
+
+def from_json_string(my_str):
+    """_summary_
+
+    Args:
+        my_obj (_type_): _description_
+    """
+    return json.loads(my_str)
