6c7a28b198251b733fcb5bfd4e8757d016f1835c
Abraham <wole1702@gmail.com>
2023-12-14 01:43:57 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""read a file to stdout
+"""
 def read_file(filename=""):
+    """python script to open and read a file to stdout
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
     with open(filename, encoding="utf-8", mode='r') as a_file:
         print(a_file.read())
