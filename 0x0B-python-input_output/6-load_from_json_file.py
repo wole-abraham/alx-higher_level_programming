75b01c81e10278dcde1de7062faa75a56683c34e
Abraham <wole1702@gmail.com>
2023-12-14 16:24:23 +0100

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Creates an object from a Json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """_summary_
+
+    Args:
+        filename (_type_): _description_
+    """
+
+    with open(filename, encoding="utf-8") as a_file:
+        obj = json.dumps(a_file.read())
+
+    return obj
