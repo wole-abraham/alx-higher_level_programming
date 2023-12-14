2dca618df433868d1724aae058bc72f83d172f57
Abraham <wole1702@gmail.com>
2023-12-14 02:41:38 +0100

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,10 +4,16 @@
 
 
 def append_write(filename="", text=""):
+    """_summary_
 
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
     with open(filename, mode='a', encoding="utf-8") as a_file:
         a_file.write(text)
         nb_text = len(text)
     return nb_text
-
-print(append_write("my_file.txt", "hey"))
