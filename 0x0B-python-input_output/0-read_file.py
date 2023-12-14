b6e9f3130f7ba2c73c2d90225280b822b4438918
Abraham <wole1702@gmail.com>
2023-12-14 01:45:28 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     Args:
         filename (str, optional): _description_. Defaults to "".
     """
-    with open(filename, encoding="utf-8", mode='r') as a_file:
+    with open(filename, encoding="utf-8") as a_file:
         print(a_file.read())
