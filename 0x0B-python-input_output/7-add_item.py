32dc0bc844973b41d33dc0525d5108cac7857838
Abraham <wole1702@gmail.com>
2023-12-14 17:45:04 +0100

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Summary"""
+
+    import json
+    import sys
+
+    i = 1
+    list1 = []
+    while i < len(sys.argv):
+        list1.append(sys.argv[i])
+        i += 1
+
+    def save_to_json_file(my_obj, filename):
+        """writes a json representation of an object to file
+
+        Args:
+            my_obj (_type_): _description_
+            filename (_type_): _description_
+        """
+        with open(filename, encoding="utf-8", mode='w') as a_file:
+            a_file.write(json.dumps(my_obj))
+
+    def load_from_json_file(filename):
+        """_summary_
+
+        Args:
+            filename (_type_): _description_
+        """
+
+        with open(filename, encoding="utf-8") as a_file:
+            obj = json.loads(a_file.read())
+
+    save_to_json_file(list1, "add_item.json")
