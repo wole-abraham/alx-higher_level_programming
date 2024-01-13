66b31ac5ec299228c7293272533406d47ba05215
abraham <wole1702@gmail.com>
2024-01-13 13:09:44 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Rectangle"""
+
 from base import Base
+
 
 class Rectangle(Base):
     """Rectangel class inherits from base class"""
@@ -14,59 +16,48 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return self.__x    
-    
+        return self.__x
+
     @x.getter
     def x(self):
         return self.__x
+
     @x.setter
     def x(self, value):
         self.__x = value
 
-
     @property
     def y(self):
-        return self.__y    
-    
+        return self.__y
+
     @y.getter
     def y(self):
         return self.__y
+
     @y.setter
     def y(self, value):
         self.__y = value
-    
-
 
     @property
     def width(self):
-        return self.__width    
-    
+        return self.__width
+
     @width.getter
     def width(self):
         return self.__width
+
     @x.setter
     def width(self, value):
         self.__width = value
 
-    
     @property
     def height(self):
-        return self.__height    
-    
+        return self.__height
+
     @height.getter
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         self.__height = value
-    
-r1 = Rectangle(10, 2)
-print(r1.id)
-
-r2 = Rectangle(2, 10)
-print(r2.id)
-
-r3 = Rectangle(10, 2, 0, 0, 12)
-print(r3.id)
-        
-    
