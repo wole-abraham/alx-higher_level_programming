cd4a95d559effc33737a7370690eb5d71555158d
abraham <wole1702@gmail.com>
2024-01-13 19:03:25 +0100

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square subclass of traingle"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, x, y, width=size, height=size)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
