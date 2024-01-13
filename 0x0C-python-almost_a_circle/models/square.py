57e49d4e2155153dfe14ecf0477526ac90511c0a
abraham <wole1702@gmail.com>
2024-01-13 21:08:13 +0100

    all

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,4 @@ class Square(Rectangle):
     """initialize parent clas attrb an"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, width=size, height=size)
-
-    def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        super().__init__(size, size, x, y, id)
