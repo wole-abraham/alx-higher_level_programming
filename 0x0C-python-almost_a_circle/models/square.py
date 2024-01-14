62e82e6a9b7ece59f4d707b5b7d04e902886986f
abraham <wole1702@gmail.com>
2024-01-14 12:38:21 +0100

    re_fixed

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,29 @@ class Square(Rectangle):
     """initialize parent clas attrb an"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """
+         return the size of the square
+
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        else:
+            self.width = value
+            self.height = value
+
+
+
