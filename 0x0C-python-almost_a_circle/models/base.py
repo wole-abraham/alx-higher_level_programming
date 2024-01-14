7b3953a4d1ce6d43fa964f7c590fd38dde81607a
abraham <wole1702@gmail.com>
2024-01-14 13:49:50 +0100

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the json string represention
         for list of dictionaries
