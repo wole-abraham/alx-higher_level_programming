87604280599f58c36d0b599b67e364845b4ff5f0
abraham <wole1702@gmail.com>
2024-01-14 14:13:53 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base():
 
         """
         with open("Rectangle.json", mode='w', encoding="utf-8") as file:
-            if list_objs:
+            if list_objs is not None:
                 convert = cls.to_json_string(list_objs)
                 file.write(json.dumps(convert))
             else:
