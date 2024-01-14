d5273ac66f903a6a2ab04cbddf05504c1e48a3b4
abraham <wole1702@gmail.com>
2024-01-14 14:08:54 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,13 @@ class Base():
 
         json_dic = json.dumps(list_dictionaries)
         return json_dic
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save the json str representation of list_obj to file
+
+        """
+        with open("Rectangle.json", mode='w', encoding="utf-8") as file:
+            convert = cls.to_json_string(list_objs)
+            file.write(json.dumps(convert))
