451f57f2ae4a9673628c6ca127e7aa8aa1be0ade
wole <wole1702@gmail.com>
2024-02-14 12:33:23 +0000

    $1

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const computeFactorial = (n) => {
+  if (isNaN(parseInt(n))) {
+    return 1; // Factorial of NaN is 1
+  } else if (n <= 1) {
+    return 1; // Base case: factorial of 0 or 1 is 1
+  } else {
+    return n * computeFactorial(n - 1); // Recursive case: multiply n by factorial of (n - 1)
+  }
+};
+
+const input = parseInt(process.argv[2]);
+console.log(computeFactorial(input));
