28b2a8b8b3c1cc1e18850902914f12830913471b
wole <wole1702@gmail.com>
2024-02-14 11:06:18 +0000

    $1

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 
-if (process.argv.length - 2 === 0) {
+const string = parseInt(process.argv[2]);
+if (isNaN(string) || process.argv.length - 2 === 0) {
   console.log('Missing number of occurences');
 } else {
-  const string = parseInt(process.argv[2]);
-
-  if (isNaN(string)) {
-    console.log('Missing number of occurences');
-  } else {
-    let t = 0;
-    while (t < string) {
-      console.log('C is fun');
-      t++;
-    }
+  let t = 0;
+  while (t < string) {
+    console.log('C is fun');
+    t++;
   }
 }
