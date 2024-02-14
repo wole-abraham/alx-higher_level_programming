fbe834f5361ebba1b29609899b230e9863fa56a6
wole <wole1702@gmail.com>
2024-02-14 09:22:58 +0000

    $1

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
-const argv = process.argv.length - 2;
-
-if (argv === 0) {
+const argv = process.argv;
+let length = 0;
+for (const _ of argv) {
+  length = _ - 2;
+}
+if (length === 0) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
