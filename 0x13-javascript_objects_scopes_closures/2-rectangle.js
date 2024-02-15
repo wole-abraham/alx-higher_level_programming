75cd96604fcdb8c7d121687b260b810f4a5f7d3a
wole <wole1702@gmail.com>
2024-02-15 00:33:35 +0000

    2-main.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,13 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
     }
   }
 }
-
 module.exports = Rectangle;
