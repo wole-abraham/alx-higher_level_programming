2b64a9a283d014b1462556e82026ca8512dfd204
wole <wole1702@gmail.com>
2024-03-01 12:09:39 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,7 @@
 """url fetcher"""
 
 if __name__ == "__main__":
-    pass
-else:
     import urllib.request as url
     with url.urlopen('https://alx-intranet.hbtn.io/status') as html:
         response = html.read()
         print(f'Body response:\n\t- type: {type(response)}\n\t- content: {response}\n\t- utf8 content: {response.decode("utf-8")}')
-
