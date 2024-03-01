fc049029e0cdb43d33dbffcf5edd754a91fbf19f
wole <wole1702@gmail.com>
2024-03-01 12:33:54 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 """url fetcher"""
 
 if __name__ == "__main__":
-    import urllib.request as url
-    with url.urlopen('https://alx-intranet.hbtn.io/status') as html:
+    import urllib as url
+    with url.request.urlopen('https://alx-intranet.hbtn.io/status') as html:
         response = html.read()
         print(f'Body response:\n\t- type: {type(response)}\n\t- content: {response}\n\t- utf8 content: {response.decode("utf-8")}')
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#gets response header value
+
+"""Python to get x-request-id value from header file """
+
+if __name__ = '__main__':
+    import sys
+    import urllib
+
+
