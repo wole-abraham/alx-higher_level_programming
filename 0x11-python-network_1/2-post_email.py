773ac2a10965ac2a2ca5d94a22ab9d849e1f15e3
wole <wole1702@gmail.com>
2024-03-02 18:15:30 +0000

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,11 @@
 
 if __name__ == '__main__':
     import urllib.request
-    import urllib.parse
     import sys
 
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
+    email = f'email={sys.argv[2]}'
+    data = email.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as file:
         this = file.read()
