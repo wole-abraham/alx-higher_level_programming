f00d1e0580d15fc546450c8bc53399043432f11c
wole <wole1702@gmail.com>
2024-03-02 18:31:59 +0000

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # sends a post request
 
-if __name__ == '__main__':
-    import urllib.request
-    import sys
+import sys
+import urllib.parse
+import urllib.request
 
+
+if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    email = f'email={email}'.encode('utf-8')
-    req = urllib.request.Request(url, email)
-    with urllib.request.urlopen(req) as file:
-        this = file.read()
-        print(this.decode('utf-8'))
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
+
