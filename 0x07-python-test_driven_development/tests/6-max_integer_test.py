f6f86c6343c612b77e609ed6057c860513b94b20
abraham <wole1702@gmail.com>
2024-03-18 08:29:57 +0100

    6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,18 +6,13 @@ import unittest
 
 res = __import__("6-max_integer").max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """ Test cases for max_integer """
 
     def test_max(self):
-        
+
         """ Test for max value """
 
         self.assertAlmostEqual(res([1, 4, 6]), 6)
         self.assertIsNone(res([]))
-
-
-
-
-    
-
