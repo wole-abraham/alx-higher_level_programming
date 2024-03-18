99fb60de2b932d2eb82a6d999978af7d4aa95dbd
abraham <wole1702@gmail.com>
2024-03-18 08:34:24 +0100

    tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,4 +15,9 @@ class TestMaxInteger(unittest.TestCase):
         """ Test for max value """
 
         self.assertAlmostEqual(res([1, 4, 6]), 6)
+        self.assertAlmostEqual(res([5, 3, 4]), 5)
+        self.assertAlmostEqual(res([3, 5, 2]), 5)
+        self.assertAlmostEqual(res([-1, 3, 5]), 5)
+        self.assertAlmostEqual(res([-1, -4, -10]), -1)
+        self.assertAlmostEqual(res([2]), 2)
         self.assertIsNone(res([]))
