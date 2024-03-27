484f3bd97ef2c7a17fc286e2740eea54654983fe
abraham <wole1702@gmail.com>
2024-03-27 07:36:53 +0100

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- creates a table 'second table' in the database hbtn_0c_0
 -- with multiple row
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUES (1, "john", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
