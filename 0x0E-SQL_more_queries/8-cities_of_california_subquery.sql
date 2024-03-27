8e67185c5729c14ecdc30c96d54a21bad51085b2
abraham <wole1702@gmail.com>
2024-03-27 16:24:20 +0100

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- sscripts that list all the cities of california that can be found in the database
-SELECT * cities WHERE state_id = (SELECT id FROM states WHERE name = 'california') ORDER BY id ASC;
+SELECT * cities WHERE state_id = (SELECT * FROM states WHERE name = 'california') ORDER BY id ASC;
 
