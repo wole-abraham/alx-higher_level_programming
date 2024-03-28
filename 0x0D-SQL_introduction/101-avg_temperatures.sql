cdc4e314546c3d52cdac2335aebae6eca33c21a2
abraham <wole1702@gmail.com>
2024-03-28 14:04:45 +0100

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- displays the average temperature 
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
 
