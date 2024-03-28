ff32af219bf8dd866d719383a9b0625aa983dd5e
abraham <wole1702@gmail.com>
2024-03-28 03:49:28 +0100

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- tables
-SELECT tv_shows.title as title, tv_show_genres.genres_id as genre_id FROM tv_show JOIN tv_show_genre ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ;
+SELECT tv_shows.title as title, tv_show_genres.genre_id as genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ;
 
