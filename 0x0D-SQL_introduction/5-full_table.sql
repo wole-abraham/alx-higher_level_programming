145d99b5364d2b460782f7634411c126b0f36352
abraham <wole1702@gmail.com>
2024-03-28 04:06:35 +0100

    sql_fixes

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table;
---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
+
