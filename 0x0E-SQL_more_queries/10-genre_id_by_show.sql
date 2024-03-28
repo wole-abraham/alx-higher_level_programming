5a8717685c38a3eef1466488a79a72e0bb63eb9f
abraham <wole1702@gmail.com>
2024-03-28 03:50:18 +0100

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
--- tables
+-- tables list data in it
 SELECT tv_shows.title as title, tv_show_genres.genre_id as genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ;
 
