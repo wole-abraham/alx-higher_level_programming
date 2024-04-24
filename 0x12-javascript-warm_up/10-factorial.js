32133557cc0bc8618dea112e6276ed6989449de7
abraham <wole1702@gmail.com>
2024-04-24 11:50:44 +0100

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,12 @@
 
 const arg = parseInt(process.argv[2]);
 
-function factorial(a){
-    return factorial(a - 1);
+function factorial (n) {
+  if (n === 1 || n === 0 || !n) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 
+console.log(factorial(arg));
