295ad5e8c4fd4eb52e08ef7750daf703ae4064d1
abraham <wole1702@gmail.com>
2024-04-26 16:14:01 +0100

    code

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,12 +1,32 @@
 #!/usr/bin/node
 
 class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 
-    constructor(w, h) {
-        if (w > 0  && h > 0){
-            this.width = w;
-            this.height = h;
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let a = '';
 
+      for (let j = 0; j < this.width; j++) {
+        a += 'X';
+      }
 
-        
+      console.log(a);
+    }
+  }
+    rotate () {
+        let temp = this.width;
+        this.width = this.height;
+        this.height = temp;
+    }
+    double () {
+        this.width = this.width * 2;
+        this.height = this.height * 2;
+    }
+
+}
+
+module.exports = Rectangle;
