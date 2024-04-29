ed16b633e4c8e17e949200897cbaccecb3c5c912
abraham <wole1702@gmail.com>
2024-04-29 02:24:08 +0100

    code

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" python script dislays all values in the states table """
+
+import sys
+import MySQLdb
+
+
+arg = sys.argv[1:]
+host = 'localhost'
+user = arg[0]
+passwd = arg[1]
+db = arg[2]
+name = arg[3]
+port = 3306
+
+db = MySQLdb.connect(host=host, user=user,
+                     passwd=passwd, db=db,
+                     port=port)
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(name))
+
+states = cur.fetchall()
+
+for state in states:
+    print(state)
