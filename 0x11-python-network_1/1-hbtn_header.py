98c3671e88c175c06e76097db3732ec41e173264
abraham <wole1702@gmail.com>
2024-05-13 23:47:33 +0100

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" python script that display """
+
+if __name__ == "__main__":
+
+    import urllib.request
+    
+    import sys
+
+    arg = sys.argv
+
+    with urllib.request.urlopen(arg[1]) as file:
+        print(file.headers['X-request-Id'])
