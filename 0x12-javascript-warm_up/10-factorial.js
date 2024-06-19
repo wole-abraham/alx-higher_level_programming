161361ec57dfb2ad826e11ecc98a0e5cdfaacb15
abrahm <wole1702@gmail.com>
2024-06-19 16:24:46 +0100

    code

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
