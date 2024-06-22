d607c3f0242eabfa350c631aedbe309f148b306e
abrahm <wole1702@gmail.com>
2024-06-22 14:00:39 +0100

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,7 @@ db = MySQLdb.connect(user=sys.argv[1],
                      passwd=sys.argv[2],
                      db=sys.argv[3],
                      host='localhost',
+                     port = '3306',
                      )
 
 cur = db.cursor()
