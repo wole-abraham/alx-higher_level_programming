1a1525b08a5a07da4b2b8aef99c53576296ad03d
abrahm <wole1702@gmail.com>
2024-06-24 15:39:08 +0100

    code

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/pytho3
+
+"""
+    pyhton script o qurty
+
+"""
+
+if __name__ == "__main__":
+
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+{password}@localhost:3306/{db_name}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for i in session.query(State).filter_by(id=1):
+        if i:
+            print(f'{i.id}: {i.name}')
+        else:
+            print("Nothing")
