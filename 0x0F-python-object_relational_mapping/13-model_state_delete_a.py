0b9f9c2814569888b3d9df14fec9fd1d3c3f80a1
abrahm <wole1702@gmail.com>
2024-06-25 15:00:03 +0100

    code

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    python script that print the states
+    of the object
+    the database in the system
+"""
+if __name__ == "__main__":
+
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+{password}@localhost:3306/{db_name}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    i = session.query(State).filter(State.name.like('%a%'))
+    for name in i:
+        session.delete(i)
+    session.commit()
