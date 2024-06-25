948f12af44027514a2c1a4a7839b66c66d1b9173
abrahm <wole1702@gmail.com>
2024-06-25 14:46:14 +0100

    code

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+    python script that print the states
+    of the object
+    the database in the system
+"""
+if __name__ == "__main__":
+
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+{password}@localhost:3306/{db_name}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    obj = State(name='Louisiana')
+    session.add(obj)
+    i = session.query(State).filter_by(id = 2).first()
+    i.name = 'New Mexico'
+    session.commit()
