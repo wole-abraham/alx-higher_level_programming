feef7a3f93a79099b575696d36d2d1bf5337900d
abrahm <wole1702@gmail.com>
2024-06-25 14:13:30 +0100

    code

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     username = argv[1]
     password = argv[2]
     db_name = argv[3]
-    state_name = argv[4]
 
     engine = create_engine(f'mysql+mysqldb://{username}:\
 {password}@localhost:3306/{db_name}')
