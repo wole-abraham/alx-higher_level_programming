33797ab116ba07fdfb747788943d40d2d45ba556
abrahm <wole1702@gmail.com>
2024-06-25 13:53:39 +0100

    code

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,25 +5,26 @@
     the database in the system
 
 """
+if __name__ == "__main__":
 
-from sys import argv
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+    from sys import argv
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-username = argv[1]
-password = argv[2]
-db_name = argv[3]
-state_name = argv[4]
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
 
-engine = create_engine(f'mysql+mysqldb://{username}:\
+    engine = create_engine(f'mysql+mysqldb://{username}:\
 {password}@localhost:3306/{db_name}')
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-i = session.query(State).filter_by(name=state_name).first()
-if i:
-    print(i.id)
-else:
-    print("Not found")
+    i = session.query(State).filter_by(name=state_name).first()
+    if i:
+        print(i.id)
+    else:
+        print("Not found")
