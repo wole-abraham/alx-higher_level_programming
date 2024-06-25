d9f52e1b2705ee55fb7199d5826c59676720c088
abrahm <wole1702@gmail.com>
2024-06-25 13:35:42 +0100

    code

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    python script that print the State Object wit the name passd argument from
+    the database in the system
+
+"""
+
+from sys import argv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+username = argv[1]
+password = argv[2]
+db_name = argv[3]
+state_name = argv[4]
+
+engine = create_engine(f'mysql+mysqldb://{username}:\
+{password}@localhost:3306/{db_name}')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+for state in session.query(State).filter_by(name=state_name):
+    print(state.id)
