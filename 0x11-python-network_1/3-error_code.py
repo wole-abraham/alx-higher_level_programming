f6067f59c2cb93dc2c33ec1d6ddfb3ac15433aa0
abrahm <wole1702@gmail.com>
2024-07-06 01:52:37 +0100

    code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-
+"""Sends a request to a given URL and displays the response body.
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
-    python scritp that takes
+import sys
+import urllib.error
+import urllib.request
 
-"""
 
 if __name__ == "__main__":
+    url = sys.argv[1]
 
-    from sys import argv
-    import urllib.request
-
-    url = argv[1]
-
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as file:
-            res = file
-
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
-    else:
-        print(res.read().decode('utf-8'))
+        print("Error code: {}".format(e.code))
